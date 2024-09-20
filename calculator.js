25f52ee04576b426386deeba3d9a2f44677f6ed9
probuildd <97763331+probuildd@users.noreply.github.com>
2024-09-19 20:58:36 -0400

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,18 +1,22 @@
-//Calculations
-//Bryan Carbajal Albarran
-let operationResults = [];
+let computationResults = [];
+
 function runCalculator() {
-    let operationResults = true;
-    while (operationResults) {
+    let continueCalculation = true;
+
+    while (continueCalculation) {
         let firstNum = prompt("Please enter your first number:");
         let operation = prompt("Now enter any of the following operators (+, -, *, /, %):");
         let secondNum = prompt("Enter your second number:");
+        
+
         if (firstNum === null || secondNum === null || operation === null) {
             break;
         }
         firstNum = parseFloat(firstNum);
         secondNum = parseFloat(secondNum);
+
         let calcResult;
+
         if (isNaN(firstNum) || isNaN(secondNum)) {
             calcResult = "Invalid input";
         } else {
@@ -37,26 +41,29 @@ function runCalculator() {
             }
         }
         if (!isNaN(calcResult) && typeof calcResult === "number") {
-            operationResults.push(calcResult);
+            computationResults.push(calcResult);
         }
         document.write("<table>");
         document.write("<tr><th>Number 1</th><th>Operator</th><th>Number 2</th><th>Result</th></tr>");
         document.write(`<tr><td>${firstNum}</td><td>${operation}</td><td>${secondNum}</td><td>${calcResult}</td></tr>`);
         document.write("</table>");
         continueCalculation = confirm("For another Calculation click 'OK'" + "\n" + "For Results click 'Cancel'");
+
     }
-    if (operationResults.length > 0) {
+    if (computationResults.length > 0) {
         displayResults();
     }
 }
 function displayResults() {
-    let minResult = Math.min(...operationResults);
-    let maxResult = Math.max(...operationResults);
-    let totalResult = operationResults.reduce((sum, value) => sum + value, 0);
-    let averageResult = totalResult / operationResults.length;
+    let minResult = Math.min(...computationResults);
+    let maxResult = Math.max(...computationResults);
+    let totalResult = computationResults.reduce((sum, value) => sum + value, 0);
+    let averageResult = totalResult / computationResults.length;
+
     document.write("<table>");
     document.write("<tr><th>Minimum</th><th>Maximum</th><th>Average</th><th>Total</th></tr>");
     document.write(`<tr><td>${minResult}</td><td>${maxResult}</td><td>${averageResult.toFixed(2)}</td><td>${totalResult}</td></tr>`);
     document.write("</table>");
 }
 runCalculator();
+
